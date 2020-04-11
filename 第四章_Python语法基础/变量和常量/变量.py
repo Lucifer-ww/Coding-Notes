ee8967335656b14d76255ee510c5cc077e0b72ac
Little(#^.^#) <59530707+Github-Programer@users.noreply.github.com>
2020-04-11 15:21:13 +0800

    Add files via upload

---
+++ 第四章_Python语法基础/变量和常量/变量.py
@@ -0,0 +1,9 @@
+# 代码文件: chapter4/src/ch4.2.1.kt
+_hello = "Hello World"
+score_for_student = 0.0
+y = 20
+x = True
+b = False
+b = 20
+print(_hello, "\n", score_for_student, "\n", y, "\n", x, "\n", b)
+print("\n")
