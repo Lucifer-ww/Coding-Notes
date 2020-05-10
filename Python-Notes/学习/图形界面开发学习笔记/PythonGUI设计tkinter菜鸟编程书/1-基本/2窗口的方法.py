5070c4c8e70dc8a9a34bc2c9e5e9a55d5bd65fb0
Github-Programer <3392446642@qq.com>
2020-05-10 16:22:15 +0800

    PVS

--- Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/1-基本/2窗口的方法.py
+++ Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/1-基本/2窗口的方法.py
@@ -7,6 +7,7 @@ root.geometry("300x160") #设置窗口大小
 root.maxsize(400, 400) #最大窗口大小，设置拖拽的最大值
 root.minsize(100, 100) #最小窗口大小，设置拖拽的最小值
 root.configure(bg="yellow") #背景颜色方法，更改背景颜色
+root.iconbitmap("APPICON.ico")
 
 root.resizable(True, True) #设置可不可以更改窗口大小，第一个参数是宽，第二个是高，True就是可拖拽
 root.mainloop()
