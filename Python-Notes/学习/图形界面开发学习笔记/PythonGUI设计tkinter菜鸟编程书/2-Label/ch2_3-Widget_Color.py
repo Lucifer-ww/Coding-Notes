6cc979ee283c7e54182f8f556204da5673624313
Github-Programer <3392446642@qq.com>
2020-05-15 20:35:14 +0800

    big change

---
+++ Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/2-Label/ch2_3-Widget_Color.py
@@ -0,0 +1,10 @@
+# coding: UTF-8
+from tkinter import *
+
+root = Tk()
+root.title("ch2_3")
+label = Label(root, text="I Like Tkinter",
+                fg ="blue", bg = "yellow")
+label.pack()
+
+root.mainloop()
