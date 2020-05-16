67e1576196b8a7561e08893443ab974d594ed860
Github-Programer <3392446642@qq.com>
2020-05-16 10:15:32 +0800

    ONTEDNFLE

--- Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/2-Label/ch2_6-anchor.py
+++ Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/2-Label/ch2_6-anchor.py
@@ -3,7 +3,8 @@ from tkinter import *
 
 root = Tk()
 root.title("ch2_6")
-label = Label(root, text = "I likt Tkinter", fg = 'blue', bg='yellow',height =3, width= 15, anchor = 'se')
+label = Label(root, text="I likt Tkinter", fg='blue',
+              bg='yellow', height=3, width=15, anchor='se')
 
 label.pack()
 root.mainloop()
