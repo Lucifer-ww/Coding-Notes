af2ea58c8ef45c56ed156eba715f2ea9348b1fe2
Github-Programer <3392446642@qq.com>
2020-05-18 16:16:03 +0800

    加入提示dz

--- Project/PYT/打字游戏Python的GUI界面/mainwindow.py
+++ Project/PYT/打字游戏Python的GUI界面/mainwindow.py
@@ -2,6 +2,8 @@
 #!/usr/bin/python3
 
 from tkinter import *
+import random
+import time
 
 root = Tk()
 root.title("打字游戏GUI")
@@ -15,6 +17,22 @@ lb = Label(root, bitmap="hourglass",
            anchor=CENTER, font=("Helvetic", 20, "bold")
            )
 # r"E:\ProgramThomas\Coding-Notes\Project\PYT\打字游戏Python的GUI界面\resource\BITMAP_TITLE.ico"
+lb2 = Label(root, bitmap = "question",
+            compound = "left",
+            text="提示：打字游戏，点击 开始 按钮即开始计时，最后点击 提交 按钮即可",
+            fg = "#FF0000", bg = "lightyellow",
+            )
+characters = [] #0~26
+for i in range(0, 50):
+    tmp = random.randint(0, 25)
+    characters.append(chr(ord('a') + tmp))
 
+print(characters)
+chstr = ""
+for i in characters:
+    chstr += i
+
+print(chstr)
 lb.pack()
+lb2.pack()
 root.mainloop()
---
+++ Project/PYT/打字游戏Python的GUI界面/test/sizeof-buf0.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    char buf0[50];
+    printf("%d\n", sizeof(buf0));
+    return 0;
+}
