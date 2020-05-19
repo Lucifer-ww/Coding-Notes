f80a7211bcb82a550cf5cec3834d78223b41bd44
Github-Programer <3392446642@qq.com>
2020-05-19 14:05:51 +0800

    :+1:

--- Project/PYT/打字游戏Python的GUI界面/mainwindow.py
+++ Project/PYT/打字游戏Python的GUI界面/mainwindow.py
@@ -5,6 +5,7 @@ from tkinter import *
 import random
 import time
 import os
+constin: str #输入汇总changliang
 timestrS=0  # 时间记录器-first
 timestrLast=0  # 时间记录器-last
 ft = r"E:\ProgramThomas\Coding-Notes\Project\PYT\打字游戏Python的GUI界面\resource\loggerstr.log"
@@ -20,6 +21,7 @@ def getStr(ent):
     tmp = ent.get()
     print("输入：{0}".format(tmp))
     fff.write("\n"+tmp)
+    constin=tmp
 
 
 def pushStart():
@@ -34,7 +36,6 @@ def pushEnd():
     print("get timestamp minx of->timtmp={0}".format(timtmp))
     timestrLast = int(timtmp)
     print("get timestamp end of->timestrLast={0}".format(timestrLast))
-    pass
 
 
 # --------------------------------
@@ -80,11 +81,15 @@ INframe = Frame(root, width=70)  # 定义输入框架
 intxtL = Label(INframe, text="输入> ")
 
 intxt = Entry(INframe, width=50, show="*")
+
+btnframe = Frame(root, width=60)
 # BUTTON 按钮    开始和结束
-btnStart = Button(root, text="开始输入", fg="blue", width=15,
+btnStart = Button(btnframe, text="开始输入", fg="blue", width=20,
                   command=pushStart)
-btnEnd = Button(root, text="输入结束", fg="blue", width=15,
+btnEnd = Button(btnframe, text="输入结束", fg="blue", width=20,
                 command=pushEnd)
+btnExit = Button(btnframe, text='退出程序', fg="red", width=20,
+                command=root.destroy)
 
 lb.grid()
 lb2.grid()
@@ -92,9 +97,11 @@ lbtip.grid()
 INframe.grid()
 intxtL.grid(row=3, column=0, sticky=W)  # 输入>Label
 intxt.grid(row=3, column=1)  # 输入框Entry th
+btnframe.grid()
 
-btnStart.grid()
-btnEnd.grid()
+btnStart.grid(row=4, column=0)
+btnEnd.grid(row=4, column=1)
+btnExit.grid(row=4, column=2)
 
 # textTip = Text(root)
 # textTip.pack(fill=BOTH, expand=True, padx=3, pady=2)
