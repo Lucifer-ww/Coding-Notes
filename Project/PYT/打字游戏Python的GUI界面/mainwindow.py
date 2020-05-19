9003cd86d9ee2f1825efbd7888f2614bea4822aa
Github-Programer <3392446642@qq.com>
2020-05-19 13:32:40 +0800

    LQBZ

--- Project/PYT/打字游戏Python的GUI界面/mainwindow.py
+++ Project/PYT/打字游戏Python的GUI界面/mainwindow.py
@@ -5,23 +5,35 @@ from tkinter import *
 import random
 import time
 import os
-timestrS="" #时间记录器-first
-timestrE = "" #时间记录器-last
+timestrS=0  # 时间记录器-first
+timestrLast=0  # 时间记录器-last
 ft = r"E:\ProgramThomas\Coding-Notes\Project\PYT\打字游戏Python的GUI界面\resource\loggerstr.log"
 '''
 def msgShow():
     label["text"] = 'I Love Python'
     label["bg"] = 'orangerad'
 '''
+
+
 def getStr(ent):
-    #返回Entry的值
-    return ent.get()
+    # 返回Entry的值
+    tmp = ent.get()
+    print("输入：{0}".format(tmp))
+    fff.write("\n"+tmp)
+
 
 def pushStart():
-    timestrS = str(int(str(time.time())))
+    timestrS=time.time()
+    pass
+
 
 def pushEnd():
-    timestrE = str(int(str(time.time())) - int(timestrS))
+    getStr(intxt)
+    timtmp = time.time() - timestrS
+    timestrLast = int(timtmp)
+    pass
+
+
 # --------------------------------
 root = Tk()
 root.title("打字游戏GUI")
@@ -35,48 +47,54 @@ lb = Label(root, bitmap="hourglass",
            anchor=CENTER, font=("Helvetic", 20, "bold")
            )
 # r"E:\ProgramThomas\Coding-Notes\Project\PYT\打字游戏Python的GUI界面\resource\BITMAP_TITLE.ico"
-lb2 = Label(root, bitmap = "question",
-            compound = "left",
+lb2 = Label(root, bitmap="question",
+            compound="left",
             text="提示：打字游戏，点击 开始 按钮即开始计时，最后点击 提交 按钮即可",
-            fg = "#FF0000", bg = "lightyellow",
+            fg="#FF0000", bg="lightyellow",
             )
 
-characters = [] #0~26
+characters = []  # 0~26
 for i in range(0, 50):
     tmp = random.randint(0, 25)
     characters.append(chr(ord('a') + tmp))
 
-#print(characters)
+# print(characters)
 chstr = ""
 for i in characters:
     chstr += i
 
 print(chstr)
-fff=open(ft, 'a+')
+fff = open(ft, 'a+')
 fff.write('\n'+chstr)
 lbtip = Label(root, bitmap="info",
               compound="left",
               text="样例" + chstr,
-              fg ="green", bg="#cccccc",
+              fg="green", bg="#cccccc",
               font=("simsun", 12), underline=True)
 # Entry cursor circle
 
-INframe=Frame(root, width=70) #定义输入框架
+INframe = Frame(root, width=70)  # 定义输入框架
 intxtL = Label(INframe, text="输入> ")
 
 intxt = Entry(INframe, width=50, show="*")
+# BUTTON 按钮    开始和结束
+btnStart = Button(root, text="开始输入", fg="blue", width=15,
+                  command=pushStart)
+btnEnd = Button(root, text="输入结束", fg="blue", width=15,
+                command=pushEnd)
 
 lb.grid()
 lb2.grid()
 lbtip.grid()
 INframe.grid()
 intxtL.grid(row=3, column=0, sticky=W)  # 输入>Label
-intxt.grid(row=3, column=1) #输入框Entry th
+intxt.grid(row=3, column=1)  # 输入框Entry th
 
-instr=getStr(intxt)
-print(instr)
-fff.write("\n"+instr)
+btnStart.grid()
+btnEnd.grid()
+
 # textTip = Text(root)
 # textTip.pack(fill=BOTH, expand=True, padx=3, pady=2)
 # textTip.insert(END, chstr)
 root.mainloop()
+print("时间：{0}".format(timestrLast))
