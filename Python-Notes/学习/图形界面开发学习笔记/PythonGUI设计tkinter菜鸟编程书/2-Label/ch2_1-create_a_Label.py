b23cb1bdcef7d2de17921abeab1e56cd88360e00
Github-Programer <3392446642@qq.com>
2020-05-26 17:59:49 +0800

    change

--- Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/2-Label/ch2_1-create_a_Label.py
+++ Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/2-Label/ch2_1-create_a_Label.py
@@ -1,11 +1,13 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/python3
-
+'''
+目的：创建一个由tkinter制作的窗口中显示一个标签
+'''
 from tkinter import *
 
-root = Tk()
-root.title("ch2_1")
-label = Label(root, text="I LIKE TKINTER")
+root = Tk() 
+root.title("ch2_1") #设置标题
+label = Label(root, text="I LIKE TKINTER") #text参数是设置文本，将这个标签存储在label变量中
 label.pack()  # 包装与定位组件
 print(type(label))  # 传回Label数据类型
 
--- Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/2-Label/ch2_4-dimensions.py
+++ Python-Notes/学习/图形界面开发学习笔记/PythonGUI设计tkinter菜鸟编程书/2-Label/ch2_4-dimensions.py
@@ -4,5 +4,6 @@ root = Tk()
 root.title("ch2_4")
 label = Label(root, text="I Like tkinter", fg="blue",
               bg="yellow", height=3, width=15)
+# 新加入的height和width参数分别是高和宽
 label.pack()
 root.mainloop()
