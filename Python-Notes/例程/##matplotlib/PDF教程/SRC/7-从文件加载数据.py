44fbb6900de7ec0b4cacced199c0296813762d7b
Thomas学编程！ <59530707+Github-Programer@users.noreply.github.com>
2020-07-08 15:54:14 +0800

    GGG

---
+++ Python-Notes/例程/##matplotlib/PDF教程/SRC/7-从文件加载数据.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import csv
+
+x = []
+y = []
+
+with open('File/example.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+
+plt.plot(x, y, label='Loaded from file!')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
---
+++ Python-Notes/例程/##matplotlib/实战/输入生成饼图/main.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+
+# 切片个数
+# 每个切片的占比
+# 每个切片的名称
+# 选择输入颜色
+
+
+def askPrint(fault):
+    choose = input("是否打印异常？(Y/N)> ")
+    if choose == "Y":
+        print(fault)
+    else:
+        pass
+
+
+tgt = int(input("输入切片个数> "))
+print("-------------------------")
+ls = list()
+index = 0
+while index != tgt:
+    try:
+        tmp = int(input("输入切片占比> "))
+        ls.append(tmp)
+    except ValueError as fault:
+        print("输入格式错误")
+        askPrint(fault)
+        continue
+    index += 1
+
+print("-------------------------")
+cols = list()
+for i in range(0, tgt):
+    cols.append(input("输入颜色> "))
+print("-------------------------")
+names = list()
+for i in range(0, tgt):
+    names.append(input("输入名称> "))
+print("输入完成……")
+from time import sleep
+sleep(0.03)
+print("存储完成.")
+sleep(0.001)
+print("分析中…………")
+print("数据拉取100%完成")
+sleep(0.5)
+print("数据分析中", end="")
+sleep(0.9)
+from pygame.display import update, flip, quit
+print("...", end="")
+print("完成")
+print("准备成像...就绪")
+plt.pie(ls, labels=names, colors=cols, startangle=90, shadow=True, autopct='%1.1f%%')
+plt.title("test")
+plt.show()
